3ac9211d48117330079a18d1815dd0f890e46a46
Samvrith <71236082+Samvrith@users.noreply.github.com>
2020-10-26 21:15:37 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -22,6 +22,13 @@ function draw()
 {
   background(255);
   
+ 
+  damage();
+  drawSprites();
+}
+
+function damage()
+{
   if (wall.x-car.x < (car.width + wall.width) / 2)
   {
     car.velocityX = 0;
@@ -42,6 +49,4 @@ function draw()
       car.shapeColor = color(0, 255, 0);
     }
   }
-  
-  drawSprites();
 }
