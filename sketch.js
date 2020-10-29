283b8dcc52e2487ace49cadc5047831e17cd23ca
Samvrith <71236082+Samvrith@users.noreply.github.com>
2020-10-28 20:49:26 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var car, wall;
+var car, wall, thickness;
 var speed, weight;
 
 function setup() 
@@ -6,13 +6,14 @@ function setup()
   createCanvas(1600,400);
   createSprite(400, 200, 50, 50);
   
-  speed = random(55, 90);
-  weight = random(400, 1500);
+  speed = random(223, 321);
+  weight = random(0, 10);
+  thickness = random(10, 40);
 
   car = createSprite(50, 200, 50, 50);
   car.velocityX = speed;
 
-  wall = createSprite(1500, 200, 60, height/2);
+  wall = createSprite(1500, 200, thickness, height/2);
   wall.shapeColor = color(80,80,80);
   
   
@@ -23,11 +24,25 @@ function draw()
   background(255);
   
  
-  damage();
+  if(hasCollided(car, wall))
+  {
+    car.velocityX = 0;
+    var damage = ((0.5 * weight * speed * speed) / (thickness * thickness * thickness));
+    if (damage > 10)
+    {
+      car.shapeColor = color(255, 0, 0);
+    }
+
+    if (damage < 10)
+    {
+      car.shapeColor = color(0, 255, 0);
+    }
+
+  }
   drawSprites();
 }
 
-function damage()
+/*function hasCollided(lbullet, wall)
 {
   if (wall.x-car.x < (car.width + wall.width) / 2)
   {
@@ -49,4 +64,17 @@ function damage()
       car.shapeColor = color(0, 255, 0);
     }
   }
+}*/
+
+function hasCollided(lbullet, lwall)
+{
+  bulletRightEdge = lbullet.x + lbullet.width;
+  wallLeftEdge = lwall.x;
+  if(bulletRightEdge >= wallLeftEdge)
+  {
+    return true;
+  }
+  return false;
+
 }
+
